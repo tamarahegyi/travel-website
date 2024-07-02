70ae8de9da7ea448d14bb6593b0e60ef18794e24
Tamara Hegyi <hegyitamara0206@gmail.com>
2024-07-02 17:33:31 +0400

    new features added

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -1,15 +1,22 @@
-import { useEffect } from 'react';
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
       <>
         <BrowserRouter>
+         <Navbar/>
           <Routes>
-            <Route path="/" element={<Navbar/>}/>
+            <Route path="/" exact element={<Home/>} />
+            <Route path="/services" element={<Services/>}/>
+            <Route path="/products" element={<Products/>}/>
+            <Route path="/sign-up" element={<SignUp/>}/>
           </Routes>
         </BrowserRouter>
       </>
